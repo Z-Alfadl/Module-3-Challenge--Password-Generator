4dee57f187688d6d69cb887124f57cfcd4e3510a
Ziad Alfadl <alfadlziad@gmail.com>
2023-06-05 15:03:01 -0400

    Add recursive function

--- README.md
+++ README.md
@@ -2,18 +2,19 @@
 
 ## Password Generator
 
-This application is designed to generate a password based on user provided inputs. The inputs are meant to set various constraints on what kind of password is generated, such as inclusion of special characters or specific lengths.
+The purpose of this app is to generate a password based on user provided inputs. The inputs are constraints on what kind of password is generated, such as determining the length of the generated password and 
+the type of characters that are to be included.
 
-## How It Works
+## Usage
 ````
 1. User clicks the "Generate Password" button.
 2. User is prompted to enter the desired length of the password, between 8 and 128 characters.
-3. If the user inputs is not a number within that range, for example, typing "P" or leaving the field blank, an alert is triggered and the function will not continue to run. 
-4. User is then presented with various prompts on wether to include: 
+3. If the user inputs is not a number within that range, for example, typing "P" or leaving the field blank, an alert is triggered and the prompt will repeat. 
+4. User is then presented with various prompts on whether to include: 
     - Uppercase and/or lowercase characters (A-Z, a-z).
     - Special characters ex (!@#$).
     - Numeric characters (0-9).
-5. If all of the prompts in 4. are false, an alert will be shown telling the user to confirm at least one of the options.
+5. If all of the prompt responses return false, an alert will be shown telling the user to confirm at least one of the options.
 6. An array is generated containing all accepted options, which is then iterated through randomly a number of times equal to the password length set in Step 2, and added to a "password" variable.  
 7. The result is then passed to a function that prints the password on the webpage.
 ````
@@ -21,3 +22,6 @@ This application is designed to generate a password based on user provided input
 ## Visual
 
 ![](Assets\PW-Generator-screenshot.png)
+
+## Source
+https://stackoverflow.com/questions/70706563/javascript-password-generator-sometimes-not-including-character-selections
--- Script/script.js
+++ Script/script.js
@@ -1,15 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Steps for password generation:
-// Step 1: Ask for desired length (8-128 characters)
-// Step 2: THEN ask to include lowercase
-// Step 3: THEN ask to include uppercase
-// Step 4: THEN ask to include numbers
-// Step 5: THEN ask to include special characters
-// Step 6: If Steps 2-5 all false, throw error
-// Step 7: 
-
+//Each object property contains a string of characters of a particular type
 const charSetOptions = {
   num: "0123456789",
   specialChar: "!@#$%&'()*+,^-./:;<=>?[]_`{~}|\\",
@@ -19,14 +11,18 @@ const charSetOptions = {
 
 
 var generatePassword = function () {
-
+  
   var charSet = "";
   var pwArray = [];
   var randPassword = "";
   // Step 1: Ask for desired length (8-128 characters)
   var pwlength = prompt('Enter the desired length of the password. Must enter a number between 8-128.');
 
-  if (pwlength >= 8 && pwlength <= 128) {
+  // If the prompt response is less than 8 or greater than 128, alert error and repeat.
+  if (pwlength < 8 || pwlength > 128) {
+    alert('ERROR: Please enter a number between 8 and 128.');
+    return generatePassword();
+  }  else {
     // Step 2: THEN ask to include uppercase characters
     var pwUpper = confirm("Include Uppercase Characters?");
     if (pwUpper) {
@@ -51,25 +47,21 @@ var generatePassword = function () {
 
     // Step 6: If Steps 2-5 all false, alert error and repeats from the start.
     if (!pwUpper && !pwLower && !pwNumeric && !pwSpecial) {
-      alert('ERROR: Please select at least one option')
-          generatePassword();
+      alert('ERROR: Please make sure to allow at least one set of characters.')
+         return generatePassword();
     }
     //Step 7: Convert password character list to array
     pwArray = Array.from(charSet);
    
-    //Step 8: Randomly select characters from the array and add to password variable
+    //Step 8: Randomly select characters from the array and assign to randPassword
     for (var i = 0; i < pwlength; i++) {
      randPassword += pwArray[Math.floor(Math.random() * pwArray.length)]
     }
     //Step 9: Return random password
-    console.log(randPassword)
     return randPassword
 
   }
-  // If selected length is less than 8 or greater than 128, sends an alert.
-  else {
-    alert('ERROR: Password must be between 8-128 characters');
-  }
+  
 
 
 }
@@ -89,8 +81,4 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-// Sources:
-//https://stackoverflow.com/questions/70706563/javascript-password-generator-sometimes-not-including-character-selections
 
-//First determine what type of characters to include
-//
